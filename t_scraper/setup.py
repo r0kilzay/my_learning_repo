ceae51899bf96ad1b5bf30e95a0ba463ac0986e8
vrajkumar <too.cool.mr3am0@gmail.com>
2021-03-04 13:02:13 -0800

    setup

---
+++ t_scraper/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+with open('requirements.txt') as requirements:
+    required = requirements.read().splitlines()
+
+setup(
+    name='twitterscraper',
+    version='1.6.1',
+    description='Tool for scraping Tweets',
+    author= "https://twitter.com/0dayman23",
+    license='MIT',
+    packages=find_packages(exclude=["build.*", "tests", "tests.*"]),
+    install_requires=required,
+    entry_points={
+        "console_scripts": [
+            "twitterscraper = twitterscraper.main:main"
+        ]
+    })
